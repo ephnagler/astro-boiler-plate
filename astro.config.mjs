e03744ea001ae50ea07939c0320c586a063c59b1
ephnagler <ephriam@protonmail.com>
2025-09-22 10:38:05 -0400

    Cloudflare Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 import { loadEnv } from "vite";
 
+import cloudflare from '@astrojs/cloudflare';
+
 const {SANITY_API_TOKEN} = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [sanity({
     projectId: 'n4olvw93',
     dataset: 'production',
@@ -18,5 +21,7 @@ export default defineConfig({
     apiVersion: "2025-09-20", 
     studioBasePath: '/studio',
     token: SANITY_API_TOKEN
-  }), react()]
+  }), react()],
+
+  adapter: cloudflare()
 });
